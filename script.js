4d93af7b331c125dd01f7e55fbcec6f3fe4aabbc
Akshita171004 <akshitakaushik17@gmail.com>
2025-05-13 22:30:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
-// your code here
+	def fibonacci(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 0
+    elif num == 2:
+        return 1
+
+    a, b = 0, 1
+    for _ in range(2, num):
+        a, b = b, a + b
+    return b
 }
 
 module.exports = fibonacci;
