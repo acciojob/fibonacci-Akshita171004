a1a34eca3fb033eff8744bb7e89fed0c1c63b8b5
Akshita171004 <akshitakaushik17@gmail.com>
2025-06-17 23:42:52 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,14 @@
+function fibonacci(num) {
+    if (num === 1) return 0;
+    if (num === 2) return 1;
+
+    let a = 0, b = 1, fib;
+
+    for (let i = 3; i <= num; i++) {
+        fib = a + b;
+        a = b;
+        b = fib;
+    }
+
+    return b;
+}
